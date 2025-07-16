56ecb76116a6fd40ce9f95b3b3d212211f37550d
ANUJ KUMAR <kumar.anuj.28082004@gmail.com>
2025-07-16 11:14:31 +0530

    Function in TS

--- src/a.ts
+++ src/a.ts
@@ -1,2 +1,16 @@
-let abc=10;
-console.log(abc);
+//TypeScript is Compiler Based
+
+let a=10;  //Inference is autodefine the datatype
+let b:String = "10";
+//let c:String c=10 Error
+console.log(a);
+console.log(b);
+
+let arr:[String,number]=["Anuj",28]
+
+function fun1(para:string):number{
+    console.log("para");
+    return 500;
+}
+
+let res:Number=fun1("Anuj")
